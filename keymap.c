d7186d3395d6adeb768e7f87af035d4dee40e9ff
Yuki <nekprograming@proton.me>
2026-02-06 16:48:34 +0100

    control escape and navi fix

--- keymap.c
+++ keymap.c
@@ -33,11 +33,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_COLEMAK_DH] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
-  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_MINS,
-  KC_LCTL,  KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                     KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V,  KC_LBRC, KC_RBRC,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                        KC_LALT, KC_LGUI, MO(_NAVI),   KC_SPC, KC_ENT, KC_BSDL, MO(_SYMBOL), KC_RALT
+  KC_ESC,          KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
+  KC_TAB,          KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_MINS,
+  LCTL_T(KC_ESC),  KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                     KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+  KC_LSFT,         KC_Z,   KC_X,    KC_C,    KC_D,    KC_V,  KC_LBRC, KC_RBRC,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                               KC_LALT, KC_LGUI, MO(_NAVI),   KC_SPC, KC_ENT, KC_BSDL, MO(_SYMBOL), KC_RALT
 ),
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -55,11 +55,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-  KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_LBRC, KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                        KC_LALT, KC_LGUI, MO(_NAVI),   KC_SPC, KC_ENT, KC_BSDL, MO(_SYMBOL), KC_RALT
+  KC_ESC,          KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
+  KC_TAB,          KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+  LCTL_T(KC_ESC),  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT,         KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_LBRC, KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                               KC_LALT, KC_LGUI, MO(_NAVI),   KC_SPC, KC_ENT, KC_BSDL, MO(_SYMBOL), KC_RALT
 ),
 /* GAMING
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -119,7 +119,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_NAVI] = LAYOUT(
-  _______, KC_DLR,   KC_PSLS, KC_PAST, KC_PMNS, KC_PSLS,                    _______, _______, _______, _______, _______, _______,
+  _______, KC_DLR,   KC_PSLS, KC_PAST, KC_PMNS, KC_PPLS,                    _______, _______, _______, _______, _______, _______,
   _______, KC_PERC,  KC_PEQL, KC_P7,   KC_P8,   KC_P9,                      _______, _______, _______, _______, KC_PGUP, KC_PGDN,
   _______, _______,  KC_P0,   KC_P4,   KC_P5,   KC_P6,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_HOME, KC_END,
   KC_LSFT, KC_LCTL,  KC_PDOT, KC_P1,   KC_P2,   KC_P3,   KC_ENT,  _______,  _______, _______, _______, _______, _______, _______,
