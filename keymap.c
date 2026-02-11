17657c21f4cd49d2a858cd5045fbd3b1f12c6abf
Yuki <nekprograming@proton.me>
2026-02-11 17:12:02 +0100

    refactoring

--- keymap.c
+++ keymap.c
@@ -16,7 +16,6 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-
 /* COLEMAK DH
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  =   |
@@ -79,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_UP,   KC_SLSH,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_LBRC, KC_RBRC,  KC_N,    KC_M,    KC_DOT, KC_LEFT, KC_DOWN, KC_RGHT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_LBRC, KC_RBRC,  KC_N,    KC_M,    KC_DOT,  KC_LEFT, KC_DOWN, KC_RGHT,
                         KC_LALT, KC_LGUI, MO(_NAVI),   KC_SPC, KC_ENT, KC_BSDL, MO(_SYMBOL), KC_RALT
 ),
 /* SYMBOL
@@ -291,16 +290,6 @@ static void render_luna(int LUNA_X, int LUNA_Y, uint8_t current_wpm, led_t led_u
         }
     }
 
-#if OLED_TIMEOUT > 0
-    /* the animation prevents the normal timeout from occuring */
-    if (last_input_activity_elapsed() > OLED_TIMEOUT && last_led_activity_elapsed() > OLED_TIMEOUT) {
-        oled_off();
-        return;
-    } else {
-        oled_on();
-    }
-#endif
-
     /* animation timer */
     if (timer_elapsed32(anim_timer) > ANIM_FRAME_DURATION) {
         anim_timer = timer_read32();
@@ -410,12 +399,24 @@ static void display_wpm(uint8_t current_wpm) {
 // When you add source files to SRC in rules.mk, you can use functions.
 bool oled_task_user(void) {
 
-    if (!oled_enabled) {
-        if (is_oled_on()) {
-            oled_off();
-            return false;
+    #if OLED_TIMEOUT > 0
+        if ((!oled_enabled) || (last_input_activity_elapsed() > OLED_TIMEOUT && last_led_activity_elapsed() > OLED_TIMEOUT)) {
+            if (is_oled_on()) {
+                oled_off();
+                return false;
+            }
         }
-    }
+    #else
+        if (!oled_enabled) {
+            if (is_oled_on()) {
+                oled_off();
+                return false;
+            }
+        }
+    #endif
+
+    // ensure activation of both oleds
+    oled_on();
 
     current_wpm   = get_current_wpm();
     led_usb_state = host_keyboard_led_state();
@@ -453,7 +454,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
 
         // Keyboard pet
-        case LCTL_T(KC_ESC)
+        case LCTL_T(KC_ESC):
         case KC_LCTL:
             if (record->event.pressed) {
                 isSneaking = true;
