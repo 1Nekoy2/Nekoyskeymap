7e3370f21f1807d7f96f8fca3c9fe950ec4a2ad5
Yuki <nekprograming@proton.me>
2026-02-15 20:56:14 +0100

    layer rework

--- keymap.c
+++ keymap.c
@@ -83,34 +83,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 /* SYMBOL
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |      |      |   /  |   *  |   -  |   +  |                    |   (  |   )  |   [  |   ]  |   {  |   }  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |   *  |   @  |   &  |   %  |   #  |   _  |
+ * |      |   $  |   =  |   7  |   8  |   9  |                    |   @  |   %  |   &  |   |  |   #  |   :  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------.    ,-------|   \  |   ^  |   ~  |   `  |   '  |   :  |
- * |------+------+------+------+------+------|   <   |    |    >  |------+------+------+------+------+------|
- * |LShift|LCTRL |      |Delete|Insert|PriScr|-------|    |-------|   /  |   (  |   )  |   [  |   ]  |   !  |
+ * |      |      |   0  |   4  |   5  |   6  |-------.    ,-------|   \  |   ^  |   ~  |   `  |   '  |   !  |
+ * |------+------+------+------+------+------| Enter |    |       |------+------+------+------+------+------|
+ * |LShift|LCTRL |   .  |   1  |   2  |   3  |-------|    |-------|   _  |   -  |   +  |   /  |   *  |   =  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |OPTION| /Space  /       \Enter \  |BackSP| BASE | RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_SYMBOL] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                 _______, _______, _______, _______, _______, _______,
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                   KC_ASTR, KC_AT,   KC_AMPR, KC_PERC, KC_HASH, KC_UNDS,
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                  KC_BSLS, KC_CIRC, KC_TILD, KC_GRV,  KC_QUOT, KC_COLN,
-  KC_LSFT, KC_LCTL, _______, KC_DEL,  KC_INS,  KC_PSCR, KC_LT,   KC_GT, KC_SLSH, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, KC_EXLM,
+  _______, _______,  KC_PSLS, KC_PAST, KC_PMNS, KC_PPLS,                   KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR,
+  _______, KC_DLR,   KC_PEQL, KC_P7,   KC_P8,   KC_P9,                     KC_AT,   KC_PERC, KC_AMPR, KC_PIPE, KC_HASH, KC_COLN,
+  _______, _______,  KC_P0,   KC_P4,   KC_P5,   KC_P6,                     KC_BSLS, KC_CIRC, KC_TILD, KC_GRV,  KC_QUOT, KC_EXLM,
+  KC_LSFT, KC_LCTL,  KC_PDOT, KC_P1,   KC_P2,   KC_P3,   KC_ENT, _______,  KC_UNDS, KC_PMNS, KC_PPLS, KC_PSLS, KC_ASTR, KC_PEQL,
                              _______, _______, _______, _______, _______, _______, _______, _______
 ),
 /* NAVI
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |   $  |   *  |   /  |   -  |   +  |                    |      |      |      |      |      |      |
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   %  |   =  |   7  |   8  |   9  |                    |      |      |      |      |PageUp|PageDn|
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |      |      |      |      |PageUp|PageDn|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |   0  |   4  |   5  |   6  |-------.    ,-------| Left | Down |  Up  |Right | Home | End  |
- * |------+------+------+------+------+------| Enter |    |       |------+------+------+------+------+------|
- * |LShift|LCTRL |   .  |   1  |   2  |   3  |-------|    |-------|      |      |      |      |      |      |
+ * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------.    ,-------| Left | Down |  Up  |Right | Home | End  |
+ * |------+------+------+------+------+------|   <   |    |    >  |------+------+------+------+------+------|
+ * |LShift|LCTRL |      |Delete|Insert|PriScr|-------|    |-------|   (  |   )  |   [  |   ]  |   {  |   }  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI | BASE | /Space  /       \Enter \  |BackSP|OPTION| RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -118,11 +118,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_NAVI] = LAYOUT(
-  _______, KC_DLR,   KC_PSLS, KC_PAST, KC_PMNS, KC_PPLS,                    _______, _______, _______, _______, _______, _______,
-  _______, KC_PERC,  KC_PEQL, KC_P7,   KC_P8,   KC_P9,                      _______, _______, _______, _______, KC_PGUP, KC_PGDN,
-  _______, _______,  KC_P0,   KC_P4,   KC_P5,   KC_P6,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_HOME, KC_END,
-  KC_LSFT, KC_LCTL,  KC_PDOT, KC_P1,   KC_P2,   KC_P3,   KC_ENT,  _______,  _______, _______, _______, _______, _______, _______,
-                             _______, _______, _______,  _______, _______,  _______, _______, _______
+  _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, _______,
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                    _______, _______, _______, _______, KC_PGUP, KC_PGDN,
+  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_HOME, KC_END,
+  KC_LSFT, KC_LCTL, _______, KC_DEL,  KC_INS,  KC_PSCR, KC_LT, KC_GT,    KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR,
+                           _______, _______, _______, _______, _______,  _______, _______, _______
 ),
 /* OPTIONS
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -332,18 +332,18 @@ static void display_active_layer(led_t led_usb_state) {
             oled_clear();
             oled_set_cursor(0, 1);
             if (led_usb_state.caps_lock) {
-                oled_write("NAVI & NUMPAD", false);
+                oled_write("NAVI & FKEYS", false);
             } else {
-                oled_write("Navi & numpad", false);
+                oled_write("Navi & fkeys", false);
             }
             break;
         case _SYMBOL:
             oled_clear();
             oled_set_cursor(0, 2);
             if (led_usb_state.caps_lock) {
-                oled_write("SYMBOLS & FKEYS", false);
+                oled_write("SYMBOLS & NUMPAD", false);
             } else {
-                oled_write("Symbols & fkeys", false);
+                oled_write("Symbols & numpad", false);
             }
             break;
         case _OPTIONS:
